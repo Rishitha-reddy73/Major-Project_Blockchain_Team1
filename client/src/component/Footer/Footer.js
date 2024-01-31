926304dab01548319b7d6082c90ff5f2519a9839
Rishitha-reddy45 <rishithagopu03@gmail.com>
2024-01-31 16:22:30 +0530

    first commit

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      
+    </div>
+  </>
+);
+
+export default Footer;
